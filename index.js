163b1bdd8a0ec2358e036cbc79c7cbba4206a0ac
Oliver Lomanat <lomanatoliver@gmail.com>
2025-06-20 07:15:50 +0300

    Adds submit functoion to index.js

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Add your code here
+
+function submitData(name, email) {
+  const userData = {
+    name: name,
+    email: email
+  };
+
+  const config = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify(userData)
+  };
+
+  return fetch("http://localhost:3000/users", config)
+    .then(response => response.json())
+    .then(data => {
+      document.body.innerHTML += `<p>${data.id}</p>`;
+    })
+    .catch(error => {
+      document.body.innerHTML += `<p>${error.message}</p>`;
+    });
+}
